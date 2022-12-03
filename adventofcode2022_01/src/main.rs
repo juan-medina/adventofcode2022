5755ac0625815900a588dd0ef60f8c631db0f131
Juan Medina <jamedina@gmail.com>
2022-12-03 20:22:19 +0000

    add to day 1 both parts and example

--- adventofcode2022_01/src/main.rs
+++ adventofcode2022_01/src/main.rs
@@ -27,15 +27,29 @@ use std::{
 };
 
 fn main() {
-    // current elf number
-    let mut current_elf_number = 1;
+    solve_day_1_part_1();
+    solve_day_1_part_2();
+}
+
+const EXAMPLE_FILE : &str =  "data/calories_example.dat";
+const PUZZLE_FILE : &str =  "data/calories_puzzle.dat";
+
+fn solve_day_1_part_1() {
+    solve_day_1("part 1 [example]", EXAMPLE_FILE, 1);
+    solve_day_1("part 1 [puzzle]", PUZZLE_FILE, 1);
+}
+fn solve_day_1_part_2() {
+    solve_day_1("part 2 [example]", EXAMPLE_FILE, 3);
+    solve_day_1("part 1 [puzzle]", PUZZLE_FILE, 3);
+}
+
+fn solve_day_1(label: &str, filename: &str, top: usize) {
     // total calories in the current elf
     let mut current_elf_total_calories = 0;
     // total calories per elf
-    let mut calories_per_elf: Vec<i32> = Vec::new();
+    let mut calories_per_elf: Vec<u32> = Vec::new();
 
     //open the file
-    let filename = "data/calories.dat";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
@@ -45,26 +59,21 @@ fn main() {
 
         // line empty, we handle previous elf totals
         if line.is_empty() {
-            // print previous elf stats
-            println!(
-                "elf number {} has {} total calories",
-                current_elf_number, current_elf_total_calories
-            );
             calories_per_elf.push(current_elf_total_calories);
             // next elf stars with 0 calories
-            current_elf_number += 1;
             current_elf_total_calories = 0;
         } else {
             // add the calories to the current elf
-            current_elf_total_calories += line.parse::<i32>().unwrap();
+            current_elf_total_calories += line.parse::<u32>().unwrap();
         }
     }
-    // sort and get the sum of the 3 higher calories
-    println!("sum of top 3 calories {}", sum_top_n_calories(calories_per_elf, 3));
+    // sort and get the sum of the n calories
+    let total_calories = sum_top_n_calories(calories_per_elf, top);
+    println!("{label} sum of top {top} calories: {total_calories}");
 }
 
 // sum the top n calories
-fn sum_top_n_calories(calories: Vec<i32>, top: usize) -> i32 {
+fn sum_top_n_calories(calories: Vec<u32>, top: usize) -> u32 {
     let mut copy = calories.clone();
     copy.sort();
     copy.iter().rev().take(top).sum()
