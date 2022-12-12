46205e4879d16ef55e24e2f71f1c06257f39f07a
Juan Medina <jamedina@gmail.com>
2022-12-12 08:13:46 +0000

    preparing for 12

---
+++ adventofcode2022_12/src/main.rs
@@ -0,0 +1,54 @@
+/***
+Copyright (c) 2022 Juan Medina
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+***/
+
+use adventofcode2022_lib::utils::{Example, RunType};
+
+const NAME: &'static str = "Hill Climbing Algorithm";
+const OUTPUT: &'static str = "steps";
+const FILE: &'static str = "heightmap";
+
+fn main() {
+    Example::new(12, NAME, OUTPUT, FILE, solve_day_12).run_all();
+}
+
+fn solve_day_12(_filename: &str, _run_type: RunType) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    use adventofcode2022_lib::utils::FileType;
+
+    #[test]
+    fn test_part_1() {
+        let example = Example::new(12, NAME, OUTPUT, FILE, solve_day_12);
+        assert_eq!(0,example.run_part(FileType::ExampleFile, RunType::Part1));
+    }
+
+    #[test]
+    fn test_part_2() {
+        let example = Example::new(12, NAME, OUTPUT, FILE, solve_day_12);
+        assert_eq!(0,example.run_part(FileType::ExampleFile, RunType::Part2));
+    }
+}
